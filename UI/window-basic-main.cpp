0748391494a3e64314c1fa688b4e091c550845bf
David R. Myers <davidrmyersii@gmail.com>
2022-01-16 14:46:01 -0500

    UI: Add ability to toggle projector border via context menu or setting
    This feature adds a "Toggle window border" option to the projector window's context (popup) menu.

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -10901,6 +10901,12 @@ void OBSBasic::UpdateProjectorHideCursor()
 		projectors[i]->SetHideCursor();
 }
 
+void OBSBasic::UpdateProjectorHideFrame(bool hideFrame)
+{
+	for (size_t i = 0; i < projectors.size(); i++)
+		projectors[i]->SetHideFrame(hideFrame);
+}
+
 void OBSBasic::UpdateProjectorAlwaysOnTop(bool top)
 {
 	for (size_t i = 0; i < projectors.size(); i++)
