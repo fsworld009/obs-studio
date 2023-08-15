0c4070ca96def29107fc23846af861325719399c
WorldFS <fsworld009@gmail.com>
2023-08-15 10:27:02 +0800

    UI: Only allow click to drag projector when in frameless mode

--- UI/window-projector.cpp
+++ UI/window-projector.cpp
@@ -369,7 +369,8 @@ void OBSProjector::mousePressEvent(QMouseEvent *event)
 
 void OBSProjector::mouseMoveEvent(QMouseEvent *event)
 {
-	if (!isFullScreen() && (event->buttons() & Qt::LeftButton)) {
+	if (!isFullScreen() && hideFrame &&
+	    (event->buttons() & Qt::LeftButton)) {
 		QPoint diff = event->pos() - onMousePressMouseOffset;
 		this->window()->move(this->window()->pos() + diff);
 	}
@@ -377,7 +378,7 @@ void OBSProjector::mouseMoveEvent(QMouseEvent *event)
 
 void OBSProjector::enterEvent(QEnterEvent *)
 {
-	if (!isFullScreen()) {
+	if (!isFullScreen() && hideFrame) {
 		setCursor(Qt::SizeAllCursor);
 	}
 }
@@ -592,10 +593,8 @@ QRect OBSProjector::GetScreenSize()
 
 std::vector<std::pair<int, int>> OBSProjector::GetResizeResolutionPresets()
 {
-	int resolutionPresets[][2] = {{1280, 720},
-				      {1920, 1080},
-				      {2560, 1440},
-				      {3840, 2160}};
+	int resolutionPresets[][2] = {
+		{1280, 720}, {1920, 1080}, {2560, 1440}, {3840, 2160}};
 	std::vector<std::pair<int, int>> availablePresets;
 
 	QRect screenSize = GetScreenSize();
